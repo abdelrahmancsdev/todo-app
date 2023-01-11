5390393a0ff84b115bb52612de0acc18aa3de645
Abdelrahman Nasser <abdelrahman.nasseryo@gmail.com>
2023-01-11 10:50:06 +0200

    Basic JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,47 @@
-import React from 'react';
+import React from "react";
 
 function App() {
   return (
     <div className="App">
-      Todo
-
-      0 items left
-
-      All
-      Active
-      Completed
-
-      Clear Completed
-
-      Drag and drop to reorder list
+      <header>
+        <h1>Todo</h1>
+        <div className="modeToggler">Dark</div>
+      </header>
+      <div id="createTodo">
+        <div id="addTodo" />
+        <input
+          type="text"
+          name="todoInput"
+          id="todoInput"
+          placeholder="Create a new todo..."
+        />
+      </div>
+      <section id="todosArea">
+        <ul className="todosList">
+          <li>
+            <p role="presentation" className="todoItem">
+              Test
+            </p>
+            <button className="deleteItem">Delete</button>
+          </li>
+        </ul>
+      </section>
+      <footer id="todoOptions">
+        <p id="itemsLeft">1 items left</p>
+        <ul id="todoFilters">
+          <li role="presentation" className="filter active">
+            All
+          </li>
+          <li role="presentation" className="filter">
+            Active
+          </li>
+          <li role="presentation" className="filter">
+            Completed
+          </li>
+        </ul>
+        <button>Clear Completed</button>
+      </footer>
+      <p id="note">Drag and drop to reorder list</p>
     </div>
   );
 }
