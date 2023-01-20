4fb557134ddc343b540d11ca5b2f98cb6a09306e
Abdelrahman Nasser <abdelrahman.nasseryo@gmail.com>
2023-01-20 17:27:22 +0200

    Enable Checking Tasks

--- src/assets/components/TodoItem.jsx
+++ src/assets/components/TodoItem.jsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useContext } from "react";
+import { TodoListContext } from "../../App";
 import s from "../styles/components/todosArea.module.scss";
 
 function TodoItem({ id, name, checked }) {
+  const { todoList, setTodoList } = useContext(TodoListContext);
+  const checkTodo = (e) => {
+    const newList = todoList.map((todo, i) => {
+      if (+e.target.parentElement.parentElement.id === i + 1) {
+        console.log("haha");
+        return { name: todo.name, checked: !todo.checked };
+      }
+      // console.log(e.target.parentElement.parentElement.id == i + 1);
+      return todo;
+    });
+    setTodoList(newList);
+    localStorage.todoList = JSON.stringify(newList);
+  };
   return (
     <li id={id} className={s.todoItem} data-checked={checked}>
-      <p role="presentation" className={s.todoTitle}>
+      <p role="presentation" className={s.todoTitle} onClick={checkTodo}>
         <button className={s.checkTodo} />
         <span className={s.todoText}>{name}</span>
       </p>
